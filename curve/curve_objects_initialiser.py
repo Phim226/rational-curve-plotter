2318bd1a5bc8a0391b658d7de87fe4767f64631b
Andrew Phimister <andyph1@hotmail.com>
2024-02-07 15:57:10 +0000

    Changed initialise_random_polynomials to an internal only function

--- curve/curve_objects_initialiser.py
+++ curve/curve_objects_initialiser.py
@@ -2,7 +2,7 @@ from curve.random_polynomial import RandPolynomial
 from curve.rational_function import RationalFunction
 from main_window.element_keys import *
 
-def initialise_random_polynomials(num_degree = None, den_degree = None):
+def _initialise_random_polynomials(num_degree = None, den_degree = None):
     return [RandPolynomial(num_degree), RandPolynomial(den_degree)]
 
 def initialise_curve_objects(values):
@@ -10,13 +10,13 @@ def initialise_curve_objects(values):
     force_num_degree, force_den_degree = values[FORCE_NUM_DEG_KEY], values[FORCE_DEN_DEG_KEY]
     forced_num_degree, forced_den_degree = values[NUM_DEG_SPIN_KEY], values[DEN_DEG_SPIN_KEY]
     if force_num_degree and not force_den_degree:
-        numerator, denominator = initialise_random_polynomials(num_degree = forced_num_degree)
+        numerator, denominator = _initialise_random_polynomials(num_degree = forced_num_degree)
     elif not force_num_degree and force_den_degree:
-        numerator, denominator = initialise_random_polynomials(den_degree = forced_den_degree)
+        numerator, denominator = _initialise_random_polynomials(den_degree = forced_den_degree)
     elif force_num_degree and force_den_degree:
-        numerator, denominator = initialise_random_polynomials(num_degree = forced_num_degree, den_degree = forced_den_degree)
+        numerator, denominator = _initialise_random_polynomials(num_degree = forced_num_degree, den_degree = forced_den_degree)
     else:
-        numerator, denominator = initialise_random_polynomials()
+        numerator, denominator = _initialise_random_polynomials()
     #change these coefficients to manually assign coefficients for debugging
     #numerator = RandPolynomial(forced_degree = 3, coefficients=None)
     #denominator = RandPolynomial(forced_degree= 1, coefficients=None)
