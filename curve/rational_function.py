d49f1738c12aa02c05904f9a9fa53ab41499318c
Andrew Phimister <andyph1@hotmail.com>
2024-02-08 02:59:05 +0000

    Calculations for stationary/inflection points now also classify them properly. Also included logic for displaying points nicely when they are integers or fractions.

--- curve/rational_function.py
+++ curve/rational_function.py
@@ -29,14 +29,16 @@ class RationalFunction():
           return sorted(discontinuities)
 
      #returns True if numerator and denominator cancel each other out and the function reduces to a constant
-     def check_if_function_reduces_to_constant(self, expression): 
-         return sp.simplify(expression).is_real
+     def check_if_function_reduces_to_constant(self, rational_expression): 
+         return sp.simplify(rational_expression).is_real
 
-     def calculate_y_intercept(self):
+     def calculate_y_intercept(self, print_exact_value, decimals = None):
           for d in self.discontinuities:
                if np.isclose(d, 0):
                     return None
-          return self.function_evaluator(0)
+          if print_exact_value:
+               return self.function_evaluator(sp.Integer(0))
+          return round(self.function_evaluator(0), decimals)
 
      def calculate_roots(self, show_complex_roots=False):
           roots = sp.solve(self.rational_expression, x)
@@ -47,31 +49,54 @@ class RationalFunction():
                if r.is_real:
                     real_roots.append(r)
           return real_roots
-          
      
-     #TODO: include logic for handling when stationary/inflection points are "nice" i.e integers or rationals so they can be listed "nicely"
-     def _calculate_nth_der_stationary_points(self, nth_der_expression, decimal_places = None):
-          stat_points = []
-          num_stat_points = 0
-          for p in sp.solve(nth_der_expression, x):
-               print(p)
-               p_eval = p.evalf()
-               if np.isclose(float(sp.im(p_eval)), 0):
-                    stat_points.append((round(sp.re(p_eval), decimal_places), round(sp.re(self.function_evaluator(p_eval)), decimal_places)))
-                    num_stat_points+=1
-          return [stat_points, num_stat_points]
+     def _get_point_on_curve(self, x_val, decimal_places, value_is_nice = False):
+          if value_is_nice:
+               return (sp.re(x_val), sp.re(self.function_evaluator(x_val)))
+          return (round(sp.re(x_val), decimal_places), round(sp.re(self.function_evaluator(x_val)), decimal_places))
+     
+     def _is_value_nice(self, val):
+          if isinstance(val, sp.Rational):
+               return True
+          return False
 
-     def calculate_stationary_points(self, decimal_places = 2):
-          stat_points, num_stat_points = self._calculate_nth_der_stationary_points(self.der_expression, decimal_places)
+     def calc_and_classify_stationary_points(self, decimal_places=None):
+          self.inflection_x_val = []
+          inflections = []
+          minima = []
+          maxima = []
+          num_stat_points=0
+          solutions = sp.solve(self.der_expression, x)
+          print("First derivative solutions", solutions)
+          for p in solutions:
+               if np.isclose(float(sp.im(p)), 0):
+                    value_is_nice = self._is_value_nice(p)
+                    second_der_p_value = float(sp.re(self.second_der_evaluator(p)))
+                    if np.isclose(second_der_p_value, 0):
+                         inflections.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
+                         self.inflection_x_val.append(float(sp.re(p)))
+                    elif second_der_p_value < 0:
+                         maxima.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
+                    elif second_der_p_value > 0:
+                         minima.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
+                    num_stat_points +=1
+          stat_points = {'Minima': minima, 'Maxima': maxima, 'Stationary inflection points': inflections}
           print("There are " + str(num_stat_points) + " stationary points")
           print("Stationary points are: ", stat_points)
           return stat_points
      
-     def calculate_inflection_points(self, decimal_places = 2):
-          inflec_points, num_inflec_points = self._calculate_nth_der_stationary_points(self.second_der_expression, decimal_places)
-          print("There are " + str(num_inflec_points) + " inflection points")
-          print("Inflection points are: ", inflec_points)
-          return inflec_points
+     def calc_non_stationary_inflection_points(self, decimal_places=None):
+          inflection_points = []
+          num_inflection_points = 0
+          solutions = sp.solve(self.second_der_expression, x)
+          print("second derivative solutions: ", solutions)
+          for p in solutions:
+               if np.isclose(float(sp.im(p)), 0) and not any(np.isclose(float(sp.re(p)), self.inflection_x_val)):
+                    inflection_points.append(self._get_point_on_curve(p.evalf(), decimal_places))
+                    num_inflection_points+=1
+          print("There are " + str(num_inflection_points) + " non-stationary inflection points")
+          print("Non-stationary inflection points are: ", inflection_points)
+          return inflection_points
      
      def calculate_derivative_coefficients(self, num_coeffs, den_coeffs):
          der_num_coeffs = np.polysub(np.polymul(np.polyder(num_coeffs), den_coeffs), np.polymul(np.polyder(den_coeffs), num_coeffs))
