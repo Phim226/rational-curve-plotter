9eacf48992d4a550c839a9a86aba1271a47724ca
Andrew Phimister <andyph1@hotmail.com>
2024-02-08 03:02:33 +0000

    Rewrote update functions to reflect new formatting of analytics section and classification of stationary points

--- main_window/analytics/analytics_section.py
+++ main_window/analytics/analytics_section.py
@@ -2,6 +2,7 @@ from main_window.element_keys import *
 import curve.curve_objects_initialiser as coi
 
 #TODO: improve formatting of roots, stationary points and inflection points (currently the raw list is displayed)
+#TODO: include options for exact or approximate values 
 #TODO: include option for user to choose decimal precision of numbers
 #TODO: include option for roots to be displayed using unicode square root and imaginary unit (\u221a and \u2148 respectively)
 #TODO: Display derivative function (with formatting options)
@@ -15,25 +16,38 @@ def _update_roots_info(window):
         window[ROOTS_KEY].update(value = "There are no real roots")
 
 def _update_y_intercept_info(window):
-    y_intercept = rational_function.calculate_y_intercept()
+    print_exact_value = True
+    y_intercept = rational_function.calculate_y_intercept(print_exact_value)
     if y_intercept is None:
         window[Y_INTERCEPT_KEY].update(value = "The curve doesn't cross the y-axis")
     else: 
         window[Y_INTERCEPT_KEY].update(value = "Curve crosses the y-axis at y = " + str(y_intercept))
 
 def _update_stationary_points_info(window):
-    stat_points = rational_function.calculate_stationary_points()
-    if stat_points:
-        window[STAT_POINTS_KEY].update(value = stat_points)
+    stat_points = rational_function.calc_and_classify_stationary_points(decimal_places=2)
+    minima = stat_points.get('Minima')
+    maxima = stat_points.get('Maxima')
+    stat_inflec_points = stat_points.get('Stationary inflection points')
+    if minima:
+        window[MINIMA_KEY].update(value = minima)
     else:
-        window[STAT_POINTS_KEY].update(value = 'There are no stationary points')
+        window[MINIMA_KEY].update(value = 'There are no minima')
+    if maxima:
+        window[MAXIMA_KEY].update(value = maxima)
+    else:
+        window[MAXIMA_KEY].update(value = 'There are no maxima')
+    if stat_inflec_points:
+        window[STAT_INFLEC_POINTS_KEY].update(value = stat_inflec_points)
+    else:
+        window[STAT_INFLEC_POINTS_KEY].update(value = 'There are no stationary inflection points')
+    return stat_inflec_points
 
-def _update_inflection_points_info(window):
-    inflec_points = rational_function.calculate_inflection_points()
+def _update_non_stat_inflection_points_info(window):
+    inflec_points = rational_function.calc_non_stationary_inflection_points(decimal_places=2)
     if inflec_points:
-        window[INFLEC_POINTS_KEY].update(value = inflec_points)
+        window[NON_STAT_INFLEC_POINTS_KEY].update(value = inflec_points)
     else:
-        window[INFLEC_POINTS_KEY].update(value = 'There are no inflection points')
+        window[NON_STAT_INFLEC_POINTS_KEY].update(value = 'There are no non-stationary inflection points')
 
 def update_analytics_section(window):
     global rational_function
@@ -41,4 +55,4 @@ def update_analytics_section(window):
     _update_roots_info(window)
     _update_y_intercept_info(window)
     _update_stationary_points_info(window)
-    _update_inflection_points_info(window)
+    _update_non_stat_inflection_points_info(window)
