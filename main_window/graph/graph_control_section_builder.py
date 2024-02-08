9ba2b00a39860e8559df335dc65373c01004e722
Andrew Phimister <andyph1@hotmail.com>
2024-02-08 17:01:45 +0000

    Changed order of buttons and added show/hide analytics section buttons

--- main_window/graph/graph_control_section_builder.py
+++ main_window/graph/graph_control_section_builder.py
@@ -7,10 +7,12 @@ def update_progress_message(window, message='', vis_bool=True):
 
 def build_graph_controls():
     return sg.Column([
-                [sg.B('Generate', key=GEN_KEY), 
-                 sg.B('Show graph', key=SHOW_KEY),
-                 sg.B('Hide graph', key=HIDE_KEY), 
-                 sg.B('Update graph (currently not working)', key=UPDATE_KEY), 
+                [sg.B('Generate', key=GEN_KEY),
+                 sg.B('Update graph (currently not working)', key=UPDATE_KEY),
+                 sg.B('Show graph', key=SHOW_GRAPH_KEY),
+                 sg.B('Hide graph', key=HIDE_GRAPH_KEY), 
+                 sg.B('Show analytics', key=SHOW_ANALYTICS_KEY),
+                 sg.B('Hide analytics', key=HIDE_ANALYTICS_KEY), 
                  sg.B('Exit', key=EXIT_KEY),
                  sg.T('', visible = False, key = PROG_KEY)]
     ])
