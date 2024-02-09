47299103166d98a0cc9de04806aef64bd75903e0
Andrew Phimister <andyph1@hotmail.com>
2024-02-09 01:39:18 +0000

    Added new tip explaining how degree spinners define maximum possible degree when force degree is unticked

--- main_window/element_tips.py
+++ main_window/element_tips.py
@@ -2,5 +2,6 @@
 RAND_GEN_TIP = ' If ticked then polynomials (up to degree 2) for the numerator and denominator are generated \n with random coefficients between the values of -10 and 10 '
 MANUAL_GEN_TIP = ' If ticked then parameters (coefficients or roots) are entered manually '
 CURVE_ASYM_TIP = ' If ticked then asymptotes of degree greater than 1 are plotted '
+RANDOM_DEGREE_SPIN_TIP = ' If "Force degree" is unticked, then this value determines the highest possible \n degree that can be randomly generated '
 FORCE_DEGREE_TIP_START = ' If ticked then the '
 FORCE_DEGREE_TIP_END = ' will have randomly generated coefficients up to the chosen degree \n (with the chosen degree coefficient never 0) '
