39fc00b80cd9f453787cf509380be3712ba34437
Andrew Phimister <andyph1@hotmail.com>
2024-02-21 12:46:23 +0000

    Changed function to disable/enable all control buttons

--- main_window/graph/graph_section_controller.py
+++ main_window/graph/graph_section_controller.py
@@ -4,8 +4,13 @@ def update_visibility_of_graph_section(window, visible):
     window[TOOLBAR_KEY].update(visible = visible)
     window[FIGURE_KEY].update(visible = visible)
 
-def update_generate_button(window, disabled):
+def update_controls(window, disabled):
     window[GEN_KEY].update(disabled = disabled)
+    window[UPDATE_KEY].update(disabled = disabled)
+    window[SHOW_GRAPH_KEY].update(disabled = disabled)
+    window[HIDE_GRAPH_KEY].update(disabled = disabled)
+    window[SHOW_ANALYTICS_KEY].update(disabled = disabled)
+    window[HIDE_ANALYTICS_KEY].update(disabled = disabled)
 
 def update_progress_message(window, message='', visible=True):
     window[PROG_KEY].update(value = message, visible = visible)
