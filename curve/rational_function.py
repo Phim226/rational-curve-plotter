a0d07555ed1482b353afa5322beee7f236ad85ea
Andrew Phimister <andyph1@hotmail.com>
2024-03-18 17:31:40 +0000

    Fixed bug where constant denominator caused exact calculations to be displayed as decimals and set analytics as class variables

--- curve/rational_function.py
+++ curve/rational_function.py
@@ -5,19 +5,29 @@ import numpy as np
 class RationalFunction():
 
      def __init__(self, numerator, denominator):
-         self.numerator = numerator
-         self.denominator = denominator
-         self.rational_expression = numerator.symbolic_expression/denominator.symbolic_expression
-         self.rational_expression_simp = sp.simplify(self.rational_expression)
-         self.der_expression = sp.diff(self.rational_expression, x)
-         self.second_der_expression = sp.diff(self.der_expression, x)
-         self.reduces_to_constant = self.check_if_function_reduces_to_constant(self.rational_expression)
-         self.function_evaluator = sp.lambdify(x, self.rational_expression, "numpy") if not self.reduces_to_constant else lambda t : (numerator.coefficients[0]*t)/(denominator.coefficients[0]*t) 
-         self.der_evaluator = sp.lambdify(x, self.der_expression, "numpy") #"numpy" argument gives lambdify function access to numpy functions backed by compiled C code, which makes execution faster (without ~1microsecond, with~10nanoseconds)
-         self.second_der_evaluator = sp.lambdify(x, self.second_der_expression, "numpy")
-         exp = self.rational_expression if not denominator.symbolic_expression.is_Rational and not denominator.symbolic_expression.is_Add else self.rational_expression_simp
-         self.discontinuities = self._find_discontinuities(exp) if not self.reduces_to_constant else []
-         
+          self.numerator = numerator
+          self.denominator = denominator
+          self.rational_expression = numerator.symbolic_expression/denominator.symbolic_expression
+          self.rational_expression_simp = sp.simplify(self.rational_expression)
+          self.der_expression = sp.diff(self.rational_expression, x)
+          self.second_der_expression = sp.diff(self.der_expression, x)
+          self.reduces_to_constant = self.check_if_function_reduces_to_constant(self.rational_expression)
+          if self.reduces_to_constant:
+               self.function_evaluator = lambda t : (numerator.coefficients[0]*t)/(denominator.coefficients[0]*t)
+          elif not self.reduces_to_constant and denominator.symbolic_expression.is_real:
+               self.function_evaluator = lambda t : (self._generate_numerator_lambda(numerator.coefficients, t))/(denominator.coefficients[0])
+          else:
+               self.function_evaluator = sp.lambdify(x, self.rational_expression, "numpy")
+          self.der_evaluator = sp.lambdify(x, self.der_expression, "numpy") #"numpy" argument gives lambdify function access to numpy functions backed by compiled C code, which makes execution faster (without ~1microsecond, with~10nanoseconds)
+          self.second_der_evaluator = sp.lambdify(x, self.second_der_expression, "numpy")
+          exp = self.rational_expression if not denominator.symbolic_expression.is_Rational and not denominator.symbolic_expression.is_Add else self.rational_expression_simp
+          self.discontinuities = self._find_discontinuities(exp) if not self.reduces_to_constant else []
+     
+     def _generate_numerator_lambda(self, numerator, t):
+          n = 0
+          for c in numerator:
+               n += c*(t**(len(numerator)-numerator.index(c)-1))
+          return n
 
      def _find_discontinuities(self, rational_expression):
           inverted_rational_exp = sp.simplify(1/rational_expression)
@@ -35,17 +45,19 @@ class RationalFunction():
      def calculate_y_intercept(self, decimals):
           for d in self.discontinuities:
                if np.isclose(d, 0):
-                    return None
+                    self.y_intercept = None
           if decimals is None:
-               return self.function_evaluator(sp.Integer(0))
-          return round(self.function_evaluator(0), decimals)
+               self.y_intercept = self.function_evaluator(sp.Integer(0))
+          else:
+               self.y_intercept = round(self.function_evaluator(0), decimals)
+          return self.y_intercept
 
      def calculate_roots(self, decimal_places, show_complex_roots=False):
-          roots = sp.solve(self.rational_expression, x)
+          self.roots = sp.solve(self.rational_expression, x)
           if show_complex_roots:
-               return roots
+               return self.roots
           real_roots =[]
-          for r in roots:
+          for r in self.roots:
                if r.is_real:
                     if decimal_places is not None:
                          r = round(r, decimal_places)
@@ -63,7 +75,7 @@ class RationalFunction():
           return False
 
      def calc_and_classify_stationary_points(self, decimal_places=None):
-          self.inflection_x_val, inflections, minima, maxima = [], [], [], []
+          self.inflection_x_val, self.inflections, self.minima, self.maxima = [], [], [], []
           num_stat_points=0
           solutions = sp.solve(self.der_expression, x)
           for p in solutions:
@@ -71,33 +83,33 @@ class RationalFunction():
                     value_is_nice = self._is_value_nice(p)
                     second_der_p_value = float(sp.re(self.second_der_evaluator(p)))
                     if np.isclose(second_der_p_value, 0):
-                         inflections.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
+                         self.inflections.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
                          self.inflection_x_val.append(float(sp.re(p)))
                     elif second_der_p_value < 0:
-                         maxima.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
+                         self.maxima.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
                     elif second_der_p_value > 0:
-                         minima.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
+                         self.minima.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
                     num_stat_points +=1
-          stat_points = {'Minima': minima, 'Maxima': maxima, 'Stationary inflection points': inflections}
+          stat_points = {'Minima': self.minima, 'Maxima': self.maxima, 'Stationary inflection points': self.inflections}
           print("There are " + str(num_stat_points) + " stationary points")
           print("Stationary points are: ", stat_points)
           return stat_points
      
      def calc_non_stationary_inflection_points(self, decimal_places=None):
-          inflection_points = []
+          self.inflection_points = []
           num_inflection_points = 0
           solutions = sp.solve(self.second_der_expression, x)
           for p in solutions:
                if np.isclose(float(sp.im(p.evalf())), 0) and not any(np.isclose(float(sp.re(p.evalf())), self.inflection_x_val)):
                     value_is_nice = self._is_value_nice(p)
                     if value_is_nice:
-                         inflection_points.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
+                         self.inflection_points.append(self._get_point_on_curve(p, decimal_places, value_is_nice))
                     else:
-                         inflection_points.append(self._get_point_on_curve(p.evalf(), decimal_places))
+                         self.inflection_points.append(self._get_point_on_curve(p.evalf(), decimal_places))
                     num_inflection_points+=1
           print("There are " + str(num_inflection_points) + " non-stationary inflection points")
-          print("Non-stationary inflection points are: ", inflection_points)
-          return inflection_points
+          print("Non-stationary inflection points are: ", self.inflection_points)
+          return self.inflection_points
      
      def calculate_derivative_coefficients(self, num_coeffs, den_coeffs):
          der_num_coeffs = np.polysub(np.polymul(np.polyder(num_coeffs), den_coeffs), np.polymul(np.polyder(den_coeffs), num_coeffs))
