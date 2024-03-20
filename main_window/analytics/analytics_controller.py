fa1bdaff62f2032c9ef9844e676287d259460c3a
Andrew Phimister <andyph1@hotmail.com>
2024-03-20 11:55:51 +0000

    Improved formatting of points

--- main_window/analytics/analytics_controller.py
+++ main_window/analytics/analytics_controller.py
@@ -1,17 +1,26 @@
 from main_window.element_keys import *
 import curve.curve_objects_initialiser as coi
 
-#TODO: improve formatting of roots, stationary points and inflection points (currently the raw list is displayed)
 #TODO: include options for exact or approximate values 
 #TODO: include option for roots to be displayed using unicode square root and imaginary unit (\u221a and \u2148 respectively)
-#TODO: Display derivative function (with formatting options)
 #TODO: Display asymptotes 
+
+def _format_points_string(points):
+    string = ''
+    first = True
+    for point in points:
+        if first:
+            first = False
+            string = f'{point}'
+        else:
+            string = f'{point}, {string}'
+    return string 
 
 def _update_roots_info(window, display_analytics_as_decimals, decimal_places) -> None:
     decimal_places = decimal_places if display_analytics_as_decimals else None
     roots = rational_function.calculate_roots(decimal_places)
     if roots:
-        window[ROOTS_KEY].update(value = roots)
+        window[ROOTS_KEY].update(value = _format_points_string(roots))
     else:
         window[ROOTS_KEY].update(value = "There are no real roots")
 
@@ -21,7 +30,7 @@ def _update_y_intercept_info(window, display_analytics_as_decimals, decimal_plac
     if y_intercept is None:
         window[Y_INTERCEPT_KEY].update(value = "The curve doesn't cross the y-axis")
     else: 
-        window[Y_INTERCEPT_KEY].update(value = "Curve crosses the y-axis at y = " + str(y_intercept))
+        window[Y_INTERCEPT_KEY].update(value = f"Curve crosses the y-axis at y = {y_intercept}")
 
 def _update_stationary_points_info(window, decimal_places) -> None:
     stat_points = rational_function.calc_and_classify_stationary_points(decimal_places=decimal_places)
@@ -29,15 +38,15 @@ def _update_stationary_points_info(window, decimal_places) -> None:
     maxima = stat_points.get('Maxima')
     stat_inflec_points = stat_points.get('Stationary inflection points')
     if minima:
-        window[MINIMA_KEY].update(value = minima)
+        window[MINIMA_KEY].update(value = _format_points_string(minima))
     else:
         window[MINIMA_KEY].update(value = 'There are no minima')
     if maxima:
-        window[MAXIMA_KEY].update(value = maxima)
+        window[MAXIMA_KEY].update(value = _format_points_string(maxima))
     else:
         window[MAXIMA_KEY].update(value = 'There are no maxima')
     if stat_inflec_points:
-        window[STAT_INFLEC_POINTS_KEY].update(value = stat_inflec_points)
+        window[STAT_INFLEC_POINTS_KEY].update(value = _format_points_string(stat_inflec_points))
     else:
         window[STAT_INFLEC_POINTS_KEY].update(value = 'There are no stationary inflection points')
     return stat_inflec_points
@@ -45,7 +54,7 @@ def _update_stationary_points_info(window, decimal_places) -> None:
 def _update_non_stat_inflection_points_info(window, decimal_places):
     inflec_points = rational_function.calc_non_stationary_inflection_points(decimal_places=decimal_places)
     if inflec_points:
-        window[NON_STAT_INFLEC_POINTS_KEY].update(value = inflec_points)
+        window[NON_STAT_INFLEC_POINTS_KEY].update(value = _format_points_string(inflec_points))
     else:
         window[NON_STAT_INFLEC_POINTS_KEY].update(value = 'There are no non-stationary inflection points')
 
