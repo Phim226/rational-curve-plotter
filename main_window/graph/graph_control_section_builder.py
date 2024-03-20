7235a77d95e09fecd56ced2e79207f5f383154b4
Andrew Phimister <andyph1@hotmail.com>
2024-03-20 23:43:11 +0000

    All buttons besides Generate and Exit disabled on startup now

--- main_window/graph/graph_control_section_builder.py
+++ main_window/graph/graph_control_section_builder.py
@@ -6,10 +6,10 @@ def build_graph_controls():
     return sg.Column([
                 [sg.B('Generate', key=GEN_KEY),
                  sg.B('Update', key=UPDATE_KEY, disabled = True, tooltip = UPDATE_TIP),
-                 sg.B('Show graph', key=SHOW_GRAPH_KEY),
-                 sg.B('Hide graph', key=HIDE_GRAPH_KEY), 
-                 sg.B('Show analytics', key=SHOW_ANALYTICS_KEY),
-                 sg.B('Hide analytics', key=HIDE_ANALYTICS_KEY), 
+                 sg.B('Show graph', disabled = True, key=SHOW_GRAPH_KEY),
+                 sg.B('Hide graph', disabled = True, key=HIDE_GRAPH_KEY), 
+                 sg.B('Show analytics', disabled = True, key=SHOW_ANALYTICS_KEY),
+                 sg.B('Hide analytics', disabled = True, key=HIDE_ANALYTICS_KEY), 
                  sg.B('Exit', key=EXIT_KEY),
                  sg.T('', visible = False, key = PROG_KEY)]
     ])
